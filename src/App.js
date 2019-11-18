6f11cbf289148a292304de737df771717379a101
auzanassdq <auzanassidqi@gmail.com>
2019-11-18 14:24:05 +0700

    hooks

--- src/App.js
+++ src/App.js
@@ -1,120 +1,119 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import ListItem from "./ListItem";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      editing: false,
-      editingIndex: null,
-      newTodo: "",
-      todos: [
-        { id: 1, name: "belajar react" },
-        { id: 2, name: "Bikin modul" }
-      ]
-    };
-  }
+function App() {
+  const [state, setState] = useState({
+    editing: false,
+    editingIndex: null,
+    newTodo: "",
+    todos: [
+      { id: 1, name: "belajar react" },
+      { id: 2, name: "Bikin modul" }
+    ]
+  });
 
-  handleChange = event => {
-    this.setState({
+  const handleChange = event => {
+    setState({
+      ...state,
       newTodo: event.target.value
     });
   };
 
-  addTodo = () => {
-    const id = this.state.todos.length + 1;
+  const addTodo = () => {
+    const id = state.todos.length + 1;
     const newTodoInput = {
       id,
-      name: this.state.newTodo
+      name: state.newTodo
     };
-
-    const todos = this.state.todos;
+  
+    const todos = state.todos;
     todos.push(newTodoInput);
-
-    this.setState({
+  
+    setState({
+      ...state,
       newTodo: "",
       todos
     });
-
-    // console.log(this.state.todos)
+  
+    // console.log(state.todos)
   };
-
-  deleteTodo = index => {
-    const todos = this.state.todos;
+  
+  const deleteTodo = index => {
+    const todos = state.todos;
     todos.splice(index, 1);
-
-    this.setState({ todos });
-    // console.log(this.state.todos)
-    // console.log(this.state.todos.length)
+  
+    setState({ ...state, todos });
+    // console.log(state.todos)
+    // console.log(state.todos.length)
   };
-
-  editTodo = index => {
-    const todo = this.state.todos[index];
-    this.setState({
+  
+  const editTodo = index => {
+    const todo = state.todos[index];
+    setState({
+      ...state,
       editing: true,
       editingIndex: index,
       newTodo: todo.name
     });
   };
 
-  updateTodo = () => {
-    const index = this.state.editingIndex;
-    const todo = this.state.todos[index];
-
-    todo.name = this.state.newTodo;
-
-    const todos = this.state.todos;
+  const updateTodo = () => {
+    const index = state.editingIndex;
+    const todo = state.todos[index];
+  
+    todo.name = state.newTodo;
+  
+    const todos = state.todos;
     todos[index] = todo;
-
-    this.setState({
+  
+    setState({
+      
       editing: false,
       editingIndex: null,
       newTodo: "",
       todos
     });
-
-    // console.log(this.state.todos)
+  
+    // console.log(state.todos)
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h2 className="text-center p-4">Todos App</h2>
-        <input
-          type="text"
-          name="input-todo"
-          className="form-control my-4"
-          placeholder="Add new todo"
-          onChange={this.handleChange}
-          value={this.state.newTodo}
-        />
-        <button
-          className="btn-success form-control mb-3"
-          onClick={this.state.editing ? this.updateTodo : this.addTodo}
-        >
-          {this.state.editing ? "Update" : "Add"}
-        </button>
-        <ul className="list-group">
-          {this.state.todos.map((item, index) => {
-            return (
-              <ListItem
-                key={item.id}
-                item={item}
-                deleteTodo={() => {
-                  this.deleteTodo(index);
-                }}
-                editTodo={() => {
-                  this.editTodo(index);
-                }}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <h2 className="text-center p-4">Todos App</h2>
+      <input
+        type="text"
+        name="input-todo"
+        className="form-control my-4"
+        placeholder="Add new todo"
+        onChange={handleChange}
+        value={state.newTodo}
+      />
+      <button
+        className="btn-success form-control mb-3"
+        onClick={state.editing ? updateTodo : addTodo}
+      >
+        {state.editing ? "Update" : "Add"}
+      </button>
+      <ul className="list-group">
+        {Array.isArray(state.todos) && state.todos.map((item, index) => {
+          return (
+            <ListItem
+              key={item.id}
+              item={item}
+              deleteTodo={() => {
+                deleteTodo(index);
+              }}
+              editTodo={() => {
+                editTodo(index);
+              }}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
